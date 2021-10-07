7d8c6635aee3dd6fbf74caf572e93e48173a0c5c
Shivam Singh <shivamfaceuit@gmail.com>
2021-10-07 20:55:13 +0530

    change in bubble sort

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -4,19 +4,25 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-      int arr[]={9,4,5,7};
+      int arr[]={1,4,5,7};
       BubbleSortt(arr);
         System.out.println(Arrays.toString(arr));
     }
 
       static void BubbleSortt(int[] arr) {
+        boolean swapped=false;
           for (int i = 0; i < arr.length; i++) {
               for (int j = 1; j < arr.length-i; j++) {
                      if (arr[j-1]>arr[j]){
                          int temp=arr[j];
                          arr[j]= arr[j-1];
                          arr[j-1]=temp;
+                         swapped=true;
                      }
+              }
+
+              if (!swapped){
+                  break;
               }
 
           }
